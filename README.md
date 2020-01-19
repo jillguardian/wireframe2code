b144b9aee31c6fe822300852a1639f8e1714aff2
Angela Guardian <jillguardian@gmail.com>
2020-01-19 19:35:02 +0800

    Initialize project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Wireframe2Code
+
+Wireframes are simple black and white layouts that outline the specific type, size, and placement of web page elements.
+They are devoid of artistic choices like fonts, logos, colours, and the like.
+Because they are easily adjustable, they are ideal for planning content placement, 
+and for identifying and solving navigation and functionality problems early in the project.
+
+When building applications, there’s a turning point where you have to start building the user interface
+from your wireframe. Typically, this process is expensive, time-consuming, tedious, and repetitive.
+But what if you could generate source code straight from your hand-drawn design mockups?
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='wireframe2code',
+    packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
+    exclude=[''],
+    version='1.0.0-SNAPSHOT',
+    description='An experimental wireframe to code tool',
+    author='Angela Guardian',
+    author_email='jillguardian@gmail.com',
+    url='https://docs.google.com/document/d/1M9vRmGvUtWRM-Th1hrMKeBZBLi4AJ7sj2X5cRXpBXrU',
+    classifiers='''
+    Programming Language :: Python
+    Programming Language :: Python :: 3
+    Operating System :: OS Independent
+    Development Status :: 3 - Alpha
+    Environment :: Console
+    Intended Audience :: Developers
+    Topic :: Scientific/Engineering :: Image Recognition
+    ''',
+    python_requires='>=3.7',
+    install_requires=[
+    ]
+)
