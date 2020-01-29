8db42cd6f1fe65c910772a8cf5e9a0587ca3bd3e
Angela Guardian <jillguardian@gmail.com>
2020-01-29 16:06:43 +0800

    Rename test cases

--- wireframe2code/test/test_wireframe.py
+++ wireframe2code/test/test_wireframe.py
@@ -12,6 +12,11 @@ def clean_wireframe_sketch():
     return cv2.imread(path)
 
 
+def cursed_wireframe_sketch():
+    path = os.path.join(os.path.dirname(__file__), 'cursed_wireframe_sketch.jpg')
+    return cv2.imread(path)
+
+
 def test_rectangle_intersection_is_symmetric():
     r1 = Rectangle(50, 50, 100, 100)
     r2 = Rectangle(75, 75, 100, 100)
@@ -19,31 +24,31 @@ def test_rectangle_intersection_is_symmetric():
     assert r2.intersection(r1) == r1.intersection(r2)
 
 
-def test_wireframe_symbols():
+def test_can_detect_symbols_correctly_from_clean_input():
     capture = Capture(clean_wireframe_sketch())
     wireframe = Wireframe(capture)
     assert len(wireframe.symbols) == 7
 
 
-def test_wireframe_basic_rows():
+def test_can_compute_smallest_symbols_by_height():
     capture = Capture(clean_wireframe_sketch())
     wireframe = Wireframe(capture)
     assert len(wireframe.basic_rows()) == 4
 
 
-def test_wireframe_basic_columns():
+def test_can_compute_smallest_symbols_by_width():
     capture = Capture(clean_wireframe_sketch())
     wireframe = Wireframe(capture)
     assert len(wireframe.basic_columns()) == 3
 
 
-def test_wireframe_row_count():
+def test_can_compute_row_count():
     capture = Capture(clean_wireframe_sketch())
     wireframe = Wireframe(capture)
     assert wireframe.row_count() == 4
 
 
-def test_wireframe_column_count():
+def test_can_compute_column_count():
     capture = Capture(clean_wireframe_sketch())
     wireframe = Wireframe(capture)
     assert wireframe.column_count() == 4
